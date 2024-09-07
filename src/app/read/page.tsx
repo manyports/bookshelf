cf7e64f0801265c33523fe964ec00335bec5536e
ера <157960666+manyports@users.noreply.github.com>
2024-09-07 15:44:46 +0500

    refactor: Update pdfjs worker source URL in page.tsx

--- src/app/read/page.tsx
+++ src/app/read/page.tsx
@@ -31,10 +31,8 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.mjs",
-  import.meta.url
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc =
+  "//unpkg.com/pdfjs-dist@4.4.168/build/pdf.worker.min.mjs";
 
 interface BookType {
   id: number;
