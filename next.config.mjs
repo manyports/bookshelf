87392d25792e443c2b8cfa606772d2d338add9e3
ера <157960666+manyports@users.noreply.github.com>
2024-09-07 15:13:58 +0500

    refactor: Exclude pdfjs-dist from babel-loader in webpack configuration

--- next.config.mjs
+++ next.config.mjs
@@ -6,18 +6,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack: (config) => {
-    config.resolve.alias['pdf.worker.min'] = false; 
-    config.module.rules.push({
-      test: /\.mjs$/,
-      include: /node_modules/,
-      use: 'babel-loader',
-      exclude: /node_modules\/pdfjs-dist/, 
-    });
-    config.resolve.alias['canvas'] = false;
-
-    return config;
-  },
 };
 
 export default nextConfig;
