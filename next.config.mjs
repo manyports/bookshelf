4c4d77be7dcd626874684e4d1052a076d03e8e1d
ера <157960666+manyports@users.noreply.github.com>
2024-09-07 15:40:09 +0500

    refactor: Update next.config.mjs to ignore TypeScript and ESLint errors during build

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Ignore TypeScript errors during build
   typescript: {
     ignoreBuildErrors: true,
   },
-  // Ignore ESLint errors during build
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Add this to ignore other build errors
   experimental: {
     forceSwcTransforms: true,
   },
   webpack: (config) => {
-    // Existing webpack configuration
     config.resolve.alias.canvas = false;
 
     return config;
