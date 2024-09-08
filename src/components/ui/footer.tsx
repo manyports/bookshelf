b410e3d4d2a23cb40282dafd87b92e70588acd74
ера <157960666+manyports@users.noreply.github.com>
2024-09-08 14:11:20 +0500

    refactor: Remove logo from footer component

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -5,7 +5,6 @@ export function Footer() {
     <footer className="border-t bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <img src="/logo.png" alt="Dana Logo" className="h-6 w-auto" />
           <p className="text-center text-sm leading-loose md:text-left">
             Проект создан во время хакатона{" "}
             <a
